b330f4d7717472594f964e54b0d225a2a564b244
glopesdev <glopesdev@users.noreply.github.com>
2020-07-09 02:05:48 +0100

    Update projects to the new csproj format

--- Bonsai.Ephys/Properties/AssemblyInfo.cs
+++ Bonsai.Ephys/Properties/AssemblyInfo.cs
@@ -1,37 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using Bonsai;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bonsai - Ephys Library")]
-[assembly: AssemblyDescription("Bonsai Ephys Library containing modules for acquisition and processing of electrophysiology signals.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Gonçalo Lopes")]
-[assembly: AssemblyProduct("Bonsai.Ephys")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2011-2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("28f12d86-8589-46a3-9ecf-823dbd5018ee")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
-[assembly: AssemblyInformationalVersion("2.5.0")]
+[assembly: XmlNamespacePrefix("clr-namespace:Bonsai.Ephys", "eph")]
+[assembly: WorkflowNamespaceIcon("Bonsai:ElementIcon.Neuro")]
